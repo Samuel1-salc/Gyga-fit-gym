5c5375d1e084cbb36aed1e0905641bcee54db09d
= <=>
2025-05-01 23:30:03 -0300

    Mudança no banco de dados de Local para Remoto

--- config/database.class.php
+++ config/database.class.php
@@ -8,33 +8,31 @@ class Database {
     private $pass;
     private $port;
 
-    private $con; //cinnection
+    private $con;
 
     function __construct() {
         $this->driver = "mysql";
-        $this->dbname = "gygafit";
+        $this->host = "maglev.proxy.rlwy.net";
+        $this->port = "42686";
+        $this->dbname = "railway";
         $this->username = "root";
-        $this->pass = "";
-        $this->port = "";
-        $this->host = "localhost";
+        $this->pass = "iNnLkZTxghKQoOdRxvcjfHVKJRatKiUM";
     }
 
     function getConexao(){
-        try{
+        try {
             $this->con = new PDO(
                 "{$this->driver}:host={$this->host};port={$this->port};dbname={$this->dbname}",
-                $this->username, 
+                $this->username,
                 $this->pass
-                
             );
 
             $this->con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
             return $this->con;
 
-        }catch(PDOException $e){
-            echo "Erro: ".$e->getMessage();
+        } catch(PDOException $e) {
+            echo "Erro: " . $e->getMessage();
         }
-        
     }
 }
